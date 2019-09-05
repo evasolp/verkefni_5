e77cadb3551de3260136874b4f62062c7537cf16
Eva Sól Pétursdóttir <evasolpetursdottir@Evas-MacBook-Air-2.local>
2019-09-05 15:58:13 +0000

    first commit

--- sequence.py
+++ sequence.py
@@ -5,3 +5,22 @@
 #Step 4: We know that the first 3 numbers are 1, 2 and 3 so if the user inputs 1, 2 or 3 we know the numbers to print out.
 #Step 5: If the user inputs 4 or higher we have to find the sum of the last 3 numbers.
 
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+num1 = 1
+num2 = 2
+num3 = 3
+counter = 1
+
+while counter <= n:
+    if counter > 3:
+        number_in_seq = num1 + num2 + num3
+        print(number_in_seq)
+        num1 = num2
+        num2 = num3
+        num3 = number_in_seq
+    else:
+        number_in_seq = counter
+        print(number_in_seq)
+    counter += 1
+
+    
